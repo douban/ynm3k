2a9fbf9cc1b0c6a0bfa650a1bedeed3e8720fea2
mijun <mijun@douban.com>
2012-09-28 16:36:13 +0800

    fix issues 1

--- runTests.sh
+++ runTests.sh
@@ -2,7 +2,7 @@
 
 XCODE_PATH=`xcode-select -print-path`
 #TRACETEMPLATE="$XCODE_PATH/Platforms/iPhoneOS.platform/Developer/Library/Instruments/PlugIns/AutomationInstrument.bundle/Contents/Resources/Automation.tracetemplate"
-TRACETEMPLATE=$(find ${XCODE_PATH%/*} -type f -name "Automation.tracetemplateT")
+TRACETEMPLATE=$(find ${XCODE_PATH%/*} -type f -name "Automation.tracetemplate")
 BASE_TEST_SCRIPT=$1
 APP_LOCATION=$2
 DEVICE_ID=$3
